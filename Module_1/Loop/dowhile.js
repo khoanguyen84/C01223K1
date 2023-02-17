8e35169cb40f36cda00d2cbaf710f38091c030d7
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-02-17 10:02:51 +0700

    game puzzle

---
+++ Module_1/Loop/dowhile.js
@@ -0,0 +1,32 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+//     if(i == 5){
+//         break;
+//     }
+// }
+
+for(let i = 1; i <= 10; i++){
+    if(i % 2 == 0){
+        continue;
+    }
+    console.log(i);
+}
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     if(i == 5){
+//         break;
+//     }
+//     i++;
+// }
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     if(i == 5){
+//         break;
+//     }
+//     i++;
+// }
+// while(i <= 10)
---
+++ Module_1/Loop/for.js
@@ -0,0 +1,8 @@
+// for(let i = 1; i <= 10; i++){
+//     document.write(i + "<br>");
+// }
+
+let i = 1
+for(; i <= 10; i++){
+    console.log(i);
+}
